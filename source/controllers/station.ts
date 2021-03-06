3a263ee38cf662cb78be72b628fc6b21b62c44d7
hassaaneforte <hassaan@eforte.net>
2021-03-06 19:27:32 +0500

    changes in test file

--- source/controllers/station.ts
+++ source/controllers/station.ts
@@ -35,26 +35,6 @@ const getWeatherAndStationData = async (at: String) => {
     } catch (error) {
         return error;
     }
-    // fetch(link + config.auth.API_KEY)
-    //     .then((res) => res.json())
-    //     .catch(function (error) {
-    //         return error;
-    //     })
-    //     .then(function (response) {
-    //         Station.findOne({ $or: [{ createdAt: at }, { createdAt: { $gt: at } }] })
-    //             .exec()
-    //             .then((stations) => {
-    //                 let obj = {
-    //                     at: at,
-    //                     stations: stations,
-    //                     weather: response
-    //                 };
-    //                 return obj ;
-    //             })
-    //             .catch((error) => {
-    //                 return error;
-    //             });
-    //     });
 };
 
 /**
@@ -79,56 +59,6 @@ const getWeatherAndStationDataByKioskId = async (at: String, id: any) => {
         } else {
             return { error: 'Cannot find data' };
         }
-        // fetch('https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=' + config.auth.API_KEY)
-        //     .then((res) => res.json())
-        //     .catch(function (error) {
-        //         return res.status(400).json({
-        //             status: 400,
-        //             data: null,
-        //             error: error.message
-        //         });
-        //     })
-        //     .then(function (response) {
-        //         Station.findOne({ $or: [{ createdAt: req.params.at }, { createdAt: { $gt: req.params.at } }] })
-        //             .exec()
-        //             .then((stations) => {
-        //                 let obj = {};
-        // let check = 0;
-        // for (let i = 0; i < stations?.data.features.length; i++) {
-        //     if (stations?.data.features[i].properties.kioskId == req.params.kioskId) {
-        //         obj = {
-        //             at: req.params.at,
-        //             station: stations?.data.features[i],
-        //             weather: response
-        //         };
-        //         check = 1;
-        //         break;
-        //     }
-        // }
-        // if (check == 1) {
-        //     return res.status(200).json({
-        //         obj: obj
-        //     });
-        // } else {
-        //     return res.status(404).json({
-        //         message: null,
-        //         error: 'Cannot find data'
-        //     });
-        // }
-        //             })
-        //             .catch((error) => {
-        //                 return res.status(500).json({
-        //                     message: null,
-        //                     error: error.message
-        //                 });
-        //             });
-        //     })
-        //     .catch((error) => {
-        //         return res.status(500).json({
-        //             message: null,
-        //             error: error.message
-        //         });
-        //     });
     } catch (err) {
         return err;
     }
