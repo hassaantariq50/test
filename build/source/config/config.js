e0924de78c9d444c0a55711f23c336cc9e3746b9
hassaaneforte <hassaan@eforte.net>
2021-03-10 15:43:26 +0500

    fixing in env variables

--- build/source/config/config.js
+++ build/source/config/config.js
@@ -13,27 +13,26 @@ var MONGO_OPTIONS = {
     poolSize: 50,
     autoIndex: false
 };
-var SECRET_KEY = process.env.SECRET_KEY;
 var AUTH = {
-    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY || '@zxcasdqwe@s123',
-    ENCRYPTION_ALGORITHM: process.env.ENCRYPTION_ALGORITHM || 'aes-256-ctr',
-    SECRET_KEY: process.env.SECRET_KEY || '@qweasd@zxc321$',
-    STATIC_JWT: process.env.STATIC_JWT || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWJiOWVhZjU3YmRiNjAwMTA5NjEzNjYiLCJpYXQiOjE2MTQ4ODA0MTl9.D3EVqPxWoL3BN6sAqRmMGhbJj1oPDzpYHESUqpSeBHo',
-    UNAUTHORIZE_JWT: process.env.UNAUTHORIZE_JWT || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWJiOWVhZjU3YmRiNjAwMTA5NjEzNjciLCJpYXQiOjE2MTQ4ODA3NDd9.xAHCJT-G5dmY4BObm-6kHdcxZq3xUjjMnkrW_iT0yFE',
-    ID: process.env.ID || '5ebb9eaf57bdb60010961366',
-    API_KEY: process.env.API_KEY || '1db98eda0fbacab7b868bc384634508e'
+    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY || '',
+    ENCRYPTION_ALGORITHM: process.env.ENCRYPTION_ALGORITHM || '',
+    SECRET_KEY: process.env.SECRET_KEY || '',
+    STATIC_JWT: process.env.STATIC_JWT || '',
+    UNAUTHORIZE_JWT: process.env.UNAUTHORIZE_JWT || '',
+    ID: process.env.ID || '',
+    API_KEY: process.env.API_KEY || ''
 };
-var MONGO_USERNAME = process.env.MONGO_USERNAME || 'dbHassaan';
-var DB_NAME = process.env.DB_NAME || "hassaan-indego-db";
+var MONGO_USERNAME = process.env.MONGO_USERNAME;
+var MONGO_URL = process.env.DB || '';
 var MONGO = {
     // host: MONGO_HOST,
     username: MONGO_USERNAME,
     options: MONGO_OPTIONS,
-    url: "mongodb+srv://dbHassaan:dbHassaan@hassaan-indego-db.fcdob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    url: MONGO_URL
 };
-var SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'https://hassaan-indego-test.herokuapp.com/';
+var SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || '';
 // const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-var SERVER_PORT = process.env.PORT || 4000;
+var SERVER_PORT = process.env.PORT || '';
 var SERVER = {
     hostname: SERVER_HOSTNAME,
     port: SERVER_PORT
